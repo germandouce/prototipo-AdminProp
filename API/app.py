cbd171ebec95aaac2197face0aabeab36cdafe8c
zzoip <zpazos@fi.uba.ar>
2025-09-15 17:03:51 -0300

    Corrección de un import en app.py del Frontend y declarada la función login (no hace nada aún) de la API

--- API/app.py
+++ API/app.py
@@ -6,6 +6,8 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/login")
+def login():
+    return 0
 
 if __name__ == "__main__":
     app.run("0.0.0.0", API_PORT, debug=DEBUG=="True")
--- Frontend/app.py
+++ Frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, requests, make_response, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from config import *
 import datetime
 import os
